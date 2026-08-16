b31446208aeab577b17f0390b5676638eb64d8dc
send2debasish <send2debasish@gmail.com>
2026-08-16 07:39:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -633,7 +633,7 @@ else:
         # DASHBOARD HEADER
         # =================================================
         st_autorefresh(
-            interval=1000,
+            interval=6000,
             key="dashboard_clock"
         )
         now = datetime.now(ZoneInfo("Asia/Kolkata"))
